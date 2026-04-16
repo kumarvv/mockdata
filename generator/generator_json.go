512aa6a2c84a0f2431d54cc197e7ec7f89cfad65
kumarvv <kumarvv@gmail.com>
2026-04-16 15:29:00 -0400

    json, sql generators

--- generator/generator_json.go
+++ generator/generator_json.go
@@ -3,64 +3,14 @@ package generator
 import (
 	"context"
 	"encoding/json"
-	"fmt"
-	"os"
-	"path/filepath"
 
-	"github.com/Pallinder/go-randomdata"
 	"github.com/pkg/errors"
-	"kumarvv.com/mockdata/models"
-	"kumarvv.com/mockdata/utils"
 )
 
-func generateJSON(ctx context.Context, config *models.Config) error {
-	path := config.Target.ToPath
-	utils.Log("[JSON] processing target path: %s", path)
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		// create directory path
-		if err = os.MkdirAll(path, os.ModePerm); err != nil {
-			return errors.Wrapf(err, "failed to create directory path: %s", path)
-		}
+func generateJSON(ctx context.Context, rows []map[string]interface{}) ([]byte, error) {
+	if b, err := json.MarshalIndent(rows, "", "  "); err != nil {
+		return nil, errors.Wrapf(err, "failed to marshal data")
+	} else {
+		return b, nil
 	}
-
-	utils.Log("[JSON] processing tables. count=%d", len(config.Tables))
-	for i, table := range config.Tables {
-		tablePath := filepath.Join(path, fmt.Sprintf("%s.json", table.Name))
-		utils.Log("[JSON] processing table(%d of %d): name=%s, filePath=%s", i+1, len(config.Tables), table.Name, tablePath)
-
-		utils.Log("generating rows: %d", table.RowCount)
-		logMarker := 0
-		rows := make([]map[string]interface{}, 0)
-		gender := utils.RandomOneOf(randomdata.Male, randomdata.Female)
-		for r := 0; r < table.RowCount; r++ {
-			row := make(map[string]interface{})
-			for _, column := range table.Columns {
-				if value, err := generateValue(ctx, &column, gender); err != nil {
-					return errors.Wrapf(err, "failed to generate value for table(%d of %d): name=%s, column=%s", i+1,
-						len(config.Tables), table.Name, column.Name)
-				} else {
-					row[column.Name] = value
-				}
-			}
-			rows = append(rows, row)
-			logMarker++
-			if logMarker%100 == 0 {
-				utils.Log("generated rows %d out of %d", logMarker, table.RowCount)
-			}
-		}
-		utils.Log("total rows generated: %d", logMarker)
-
-		// write file
-		utils.Log("creating json file at path=%s", tablePath)
-		if b, err := json.MarshalIndent(rows, "", "  "); err != nil {
-			return errors.Wrapf(err, "failed to marshal json file: %s", tablePath)
-		} else {
-			if err = os.WriteFile(tablePath, b, os.ModePerm); err != nil {
-				return errors.Wrapf(err, "failed to write file: %s", tablePath)
-			}
-		}
-		utils.Log("[JSON] data file created at %s", tablePath)
-	}
-
-	return nil
 }
---
+++ generator/generator_sql.go
@@ -0,0 +1,45 @@
+package generator
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"strings"
+
+	"kumarvv.com/mockdata/models"
+)
+
+const insertTemplate = `INSER INTO %s 
+	(%s) 
+VALUES 
+	(%s); `
+
+func generateSQL(ctx context.Context, table *models.ConfigTable, rows []map[string]interface{}) ([]byte, error) {
+	contents := ""
+
+	columnNames := make([]string, 0)
+	for _, column := range table.Columns {
+		columnNames = append(columnNames, strings.ToLower(column.Name))
+	}
+	columnNamesStr := strings.Join(columnNames, ", ")
+
+	for _, row := range rows {
+		values := make([]string, 0)
+		for _, columnName := range columnNames {
+			value := row[columnName]
+			valueStr := ""
+			if reflect.TypeOf(value).Kind() == reflect.String {
+				valueStr = fmt.Sprintf("'%s'", value)
+			} else {
+				valueStr = fmt.Sprintf("%v", value)
+			}
+			values = append(values, valueStr)
+		}
+		allValues := strings.Join(values, ", ")
+
+		sql := fmt.Sprintf(insertTemplate, strings.ToLower(table.Name), columnNamesStr, allValues)
+		contents += fmt.Sprintf("%s\n", sql)
+	}
+
+	return []byte(contents), nil
+}
