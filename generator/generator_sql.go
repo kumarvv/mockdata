d3a6059f6cfd1ac2df361bed3fddc2662966b6ac
kumarvv <kumarvv@gmail.com>
2026-04-16 21:02:15 -0400

    generator sql refactor

--- generator/generator_sql.go
+++ generator/generator_sql.go
@@ -16,32 +16,43 @@ VALUES
 
 func generateSQL(ctx context.Context, table *models.ConfigTable, rows []map[string]interface{}) ([]byte, error) {
 	contents := ""
+	columnNames := generateSQLColumns(table)
 
+	for _, row := range rows {
+		if insertSql, err := generateSQLInsert(table, row, columnNames); err != nil {
+			return nil, err
+		} else {
+			contents += fmt.Sprintf("%s\n", insertSql)
+		}
+	}
+
+	return []byte(contents), nil
+}
+
+func generateSQLColumns(table *models.ConfigTable) string {
 	columnNames := make([]string, 0)
 	for _, column := range table.Columns {
 		columnNames = append(columnNames, strings.ToLower(column.Name))
 	}
-	columnNamesStr := strings.Join(columnNames, ", ")
+	return strings.Join(columnNames, ", ")
+}
 
-	for _, row := range rows {
-		values := make([]string, 0)
-		for _, columnName := range columnNames {
-			value := row[columnName]
-			valueStr := ""
-			if value == nil {
-				value = "NULL"
-			} else if reflect.TypeOf(value).Kind() == reflect.String {
-				valueStr = fmt.Sprintf("'%s'", value)
-			} else {
-				valueStr = fmt.Sprintf("%v", value)
-			}
-			values = append(values, valueStr)
+func generateSQLInsert(table *models.ConfigTable, row map[string]interface{}, columnNames string) (string, error) {
+	values := make([]string, 0)
+	for _, column := range table.Columns {
+		value := row[column.Name]
+		valueStr := ""
+		if value == nil {
+			value = "NULL"
+		} else if reflect.TypeOf(value).Kind() == reflect.String {
+			valueStr = fmt.Sprintf("'%s'", value)
+		} else {
+			valueStr = fmt.Sprintf("%v", value)
 		}
-		allValues := strings.Join(values, ", ")
-
-		sql := fmt.Sprintf(insertTemplate, strings.ToLower(table.Name), columnNamesStr, allValues)
-		contents += fmt.Sprintf("%s\n", sql)
+		values = append(values, valueStr)
 	}
+	allValues := strings.Join(values, ", ")
 
-	return []byte(contents), nil
+	sql := fmt.Sprintf(insertTemplate, strings.ToLower(table.Name), columnNames, allValues)
+	return sql, nil
 }
